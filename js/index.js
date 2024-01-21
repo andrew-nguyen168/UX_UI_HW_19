44c530b945a82cb5900a0bee1a523752f8b6e2b3
anduhru168 <ngu3121@gmail.com>
2024-01-21 20:26:58 +1100

    Added interactions

--- js/index.js
+++ js/index.js
@@ -2,15 +2,34 @@
 $("#toTop").click(function() {
     $("html, body").animate({scrollTop: 0}, 1000);
 });
-// Scroll to myWork section
+// Scroll to myWork Section
 $("#toWork").click(function() {
     $("html, body").animate({scrollTop: $(".myWork").offset().top
 }, 2000);
 });
-// Change text colour on hover
-$("#introduction a").hover(function() {
-    $(this).css("color", "blue");
-    $(this).css("text-decoration", "underline");
-
+// Scroll to About Me Section
+$("#about").click(function() {
+    $("html, body").animate({scrollTop: $(".aboutSection").offset().top
+}, 2000);
 });
+// Scroll to Skills Section
+$("#skills").click(function() {
+    $("html, body").animate({scrollTop: $(".mySkills").offset().top
+}, 2000);
+});
+// Scroll to My Work Section
+$("#work").click(function() {
+    $("html, body").animate({scrollTop: $(".myWork").offset().top
+}, 2000);
+});
+//Zoom into images on hover
+
+
+
+// Change text colour on hover
+// $("#introduction a").hover(function() {
+//     $(this).css("color", "blue");
+//     $(this).css("text-decoration", "underline");
+
+// });
 // Hamburger Menu
